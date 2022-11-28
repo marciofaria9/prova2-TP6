36533bfbefeb6a6568d4d12b049b792966a97102
Marcio Faria <m.faria.jr@icloud.com>
2022-11-28 18:42:23 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = require('./router/#.route')
+
+const app = express()
+app.use(express.json())
+app.use('/api/v1/#', router)
+
+const port = 8087 
+app.listen(port, () => console.log(`Server up on port ${port}`))
+
+//
